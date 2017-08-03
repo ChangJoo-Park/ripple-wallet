1f81ed88b6b168002e9df8c5d2231fbc3572c55a
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2017-08-03 22:00:43 +0900

    :heavy_plus_sign: Wallet Component

---
+++ src/components/pages/Wallet.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+  <div class="">
+    <header>
+      <h1>Balance</h1>
+    </header>
+    <div class="balance-information">
+      <span class="number">0</span>
+      <span class="unit">XRP</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss">
+
+</style>
